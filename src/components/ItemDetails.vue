b0f9e9f1ad118585e0035f301b00fb6c00af4773
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-22 15:58:39 +0100

    Added ItemDetails component

---
+++ src/components/ItemDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mt-10 md:flex md:gap-8">
+    <div class="rounded-xl overflow-hidden md:w-2/4">
+      <img class="w-full h-full object-cover" :src="image" :alt="name" />
+    </div>
+    <div class="flex flex-col md:w-2/4">
+      <div>
+        <h2 class="mt-2 text-xl font-semibold sm:text-2xl sm:mt-0">{{ name }}</h2>
+        <p class="mt-4"><strong>Description:</strong> {{ description }}</p>
+      </div>
+      <div class="flex justify-between items-center mt-8 md:mt-auto">
+        <span>{{ formatPrice(price) }}</span>
+        <button
+          class="inline-block bg-blue-200 text-blue-900 ml-2 px-4 py-2 rounded-md hover:bg-blue-300"
+        >
+          Ajouter
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemDetails",
+  props: ["name", "image", "description", "price"],
+  methods: {
+    formatPrice(price) {
+      return (price / 100).toFixed(2) + " €"
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,6 +1,7 @@
 <template>
-  <div v-if="item" class="item-detail">
+  <div v-if="item">
     <h1>Détail de l'article "<strong>{{item.name}}</strong>":</h1>
+    <ItemDetails :name="item.name" :image="item.imageUrl" :description="item.description" :price="item.price" />
   </div>
   <div v-else>
     Loading...
@@ -8,8 +9,13 @@
 </template>
 
 <script>
+import ItemDetails from '@/components/ItemDetails.vue'
+
 export default {
   name: 'Product',
+  components: {
+    ItemDetails
+  },
   data() {
     return {
       itemId: this.$route.params.id,
