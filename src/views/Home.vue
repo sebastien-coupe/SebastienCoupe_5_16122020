d0f88f04efb1b48dab160b89fb39b88401588ace
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-22 12:58:47 +0100

    Fetched data from api using axios in home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="home">
-    Home
+    <h1>Nos articles:</h1>
+    <div v-if="items.length">
+      <h2 v-for="item in items" :key="item">{{ item.name }}</h2>
+    </div>
+    <div v-else>
+      No data
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    fetchItems() {
+      this.$http
+        .get('http://localhost:3000/api/teddies')
+        .then(response => this.items = response.data)
+        .catch(error => console.error(error))
+    }
+  },
+  mounted() {
+    this.fetchItems()
+  }
 }
 
 </script>
