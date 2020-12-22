1ef8ef656d052530268cdf0f9c25105c5051bdba
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-22 12:23:55 +0100

    Added navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="bg-gray-800 py-4">
+    <div class="container max-w-screen-lg px-4 mx-auto">
+      <router-link class="inline-block" :to="{ name: 'Home' }">
+        <img
+          class="w-24 sm:w-32 md:w-auto relative mt-2"
+          src="../assets/orinoco.webp"
+          alt="Orinoco logo"
+          width="163"
+          height="40"
+        />
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+</style>
