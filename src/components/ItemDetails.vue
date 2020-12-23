6c8af04a94daafa815a7e16d461d52e2f27077d6
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-23 15:15:53 +0100

    Changed price style

--- src/components/ItemDetails.vue
+++ src/components/ItemDetails.vue
@@ -8,8 +8,8 @@
         <h2 class="mt-2 text-xl font-semibold sm:text-2xl sm:mt-0">{{ name }}</h2>
         <p class="mt-4"><strong>Description:</strong> {{ description }}</p>
       </div>
-      <div class="flex justify-between items-center mt-8 md:mt-auto">
-        <span>{{ formatPrice(price) }}</span>
+      <div class="flex justify-between mt-8 md:mt-auto">
+        <span class="inline-flex flex-1 items-center justify-center bg-white rounded-md text-xl text-2xl font-semibold">{{ formatPrice(price) }}</span>
         <button
           class="inline-block bg-blue-200 text-blue-900 ml-2 px-4 py-2 rounded-md hover:bg-blue-300"
         >
--- src/components/ItemPreview.vue
+++ src/components/ItemPreview.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="item">
-    <h2 class="mb-2 text-xl font-semibold">Peluche "{{ name }}"</h2>
+    <h2 class="ml-2 mb-2 text-xl font-semibold">Peluche "{{ name }}"</h2>
     <div class="image-responsive rounded-xl overflow-hidden">
       <img
         class="absolute inset-0 h-full w-full object-cover"
@@ -8,10 +8,10 @@
         :alt="name"
       />
     </div>
-    <div class="flex items-center mt-6">
-      <span class="text-xl font-semibold">{{ formatPrice(price) }}</span>
+    <div class="flex mt-6">
+      <span class="inline-flex flex-1 items-center justify-center bg-white rounded-md text-xl sm:text-2xl font-semibold">{{ formatPrice(price) }}</span>
       <router-link
-        class="inline-block ml-auto px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
+        class="inline-block ml-2 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
         :to="{ name: 'Product', params: { id: itemId } }"
         >Détails</router-link
       >
