7adef1f4ecbbc965969a1cee42ac737336bf3e98
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-23 16:33:59 +0100

    Added footer component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <Navbar />
-  <main class="text-gray-700 py-8 md:py-16">
-    <div class="container max-w-screen-lg px-4 mx-auto">
-      <router-view />
-    </div>
-  </main>
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+    <main class="flex-1 text-gray-700 py-8 md:py-16">
+      <div class="container max-w-screen-lg px-4 mx-auto">
+        <router-view />
+      </div>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="mt-auto py-4 bg-gray-200 text-sm">
+    <div class="container max-w-screen-lg sm:flex justify-between items-center px-4 mx-auto text-center">
+      <span class="font-semibold">Orinoco &copy; 2020</span>
+      <ul class="mt-4 sm:mt-0 sm:flex">
+        <li class="py-2 sm:py-0 "><a class="hover:underline" href="#">Livraison</a></li>
+        <li class="py-2 sm:py-0 sm:ml-4"><a class="hover:underline" href="#">Mentions Légales</a></li>
+        <li class="py-2 sm:py-0 sm:ml-4"><a class="hover:underline" href="#">Conditions Générales</a></li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+</style>
