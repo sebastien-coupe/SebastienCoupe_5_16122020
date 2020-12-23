6d11813f78f3b4480220232f4687ddd459f62cf9
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-23 16:07:41 +0100

    Fixed formatting

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,43 +1,47 @@
 <template>
   <div v-if="item">
-    <h1 class="text-xl sm:text-2xl font-semibold text-center uppercase">Détail de l'article "<strong>{{item.name}}</strong>"</h1>
-    <ItemDetails class="mt-10 md:mt-20" :name="item.name" :image="item.imageUrl" :description="item.description" :price="item.price" />
+    <h1 class="text-xl sm:text-2xl font-semibold text-center uppercase">
+      Détail de l'article "<strong>{{ item.name }}</strong
+      >"
+    </h1>
+    <ItemDetails
+      class="mt-10 md:mt-20"
+      :name="item.name"
+      :image="item.imageUrl"
+      :description="item.description"
+      :price="item.price"
+    />
   </div>
-  <div v-else>
-    Loading...
-  </div>
+  <div v-else>Loading...</div>
 </template>
 
 <script>
-import ItemDetails from '@/components/ItemDetails.vue'
+import ItemDetails from "@/components/ItemDetails.vue";
 
 export default {
-  name: 'Product',
+  name: "Product",
   components: {
-    ItemDetails
+    ItemDetails,
   },
   data() {
     return {
       itemId: this.$route.params.id,
-      item: null
-    }
+      item: null,
+    };
   },
   methods: {
     fetchItem(id) {
       this.$http
         .get(`http://localhost:3000/api/teddies/${id}`)
-        .then(response => this.item = response.data)
-        .catch(error => console.log(error))
-    }
+        .then((response) => (this.item = response.data))
+        .catch((error) => console.log(error));
+    },
   },
   mounted() {
-    this.fetchItem(this.itemId)
-  }
-  
-
-}
+    this.fetchItem(this.itemId);
+  },
+};
 </script>
 
 <style>
-
 </style>
