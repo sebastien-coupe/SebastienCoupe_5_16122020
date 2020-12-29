b0ad9cb455108921abd4246e38e849f80a177298
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-29 16:24:55 +0100

    Added Vuex as dependancy

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import './index.css'
 import router from './router'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
+import store from './store'
 
-createApp(App)
+createApp(App).use(store)
   .use(router)
   .use(VueAxios, axios)
   .mount('#app')
