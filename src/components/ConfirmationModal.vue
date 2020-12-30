b3aeaca305b3958a483080cf6effeee793a3729e
Sebastien Coupe <sebastien.coupe@protonmail.com>
2020-12-30 16:27:57 +0100

    Added link to checkout page in modal

--- src/components/ConfirmationModal.vue
+++ src/components/ConfirmationModal.vue
@@ -1,15 +1,20 @@
 <template>
   <div
-    class="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+    class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
   >
-    <div class="bg-white rounded-md px-8 py-4">
-      <p class="text-lg font-semibold">L'article a bien été ajouté</p>
-      <div class="text-center mt-4">
+    <div class="bg-white rounded-md p-8">
+      <div class="text-lg font-semibold">L'article a bien été ajouté</div>
+      <div class="flex flex-col sm:flex-row gap-2 mt-6">
+        <router-link
+          :to="{ name: 'Checkout' }"
+          class="inline-block px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
+          >Aller au panier</router-link
+        >
         <button
           @click="closeModal"
-          class="bg-blue-200 text-blue-900 ml-2 px-4 py-2 rounded-md"
+          class="inline-block bg-blue-200 text-blue-900 px-4 py-2 rounded-md hover:bg-blue-300"
         >
-          OK
+          Poursuivre mes achats
         </button>
       </div>
     </div>
